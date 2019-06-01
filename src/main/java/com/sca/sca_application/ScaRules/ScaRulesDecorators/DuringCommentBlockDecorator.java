6fca9383b92c0a1ce6a44703e072e33ffe70e138
razsh <razshlomo17@gmail.com>
2019-06-01 11:31:58 +0300

    bug fix

--- src/main/java/com/sca/sca_application/ScaRules/ScaRulesDecorators/DuringCommentBlockDecorator.java
+++ src/main/java/com/sca/sca_application/ScaRules/ScaRulesDecorators/DuringCommentBlockDecorator.java
@@ -4,12 +4,15 @@ import com.sca.sca_application.ScaFileInformation.ScaFileInformation;
 import com.sca.sca_application.ScaRules.ScaRule;
 import com.sca.sca_application.ScaRules.ScaRulesResults.ScaRuleInspectionResult;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
  * Decorator for handling rules that inspect files that are during Comment-Block.
  */
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class DuringCommentBlockDecorator implements ScaRule {
 
     private boolean duringCommentBlock;
